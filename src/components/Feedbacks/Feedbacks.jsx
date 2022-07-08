41c098fd1a638672a2e2293d789895dc0869353f
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-07-08 11:46:19 +0300

    optimized with one useState instead five

--- src/components/Feedbacks/Feedbacks.jsx
+++ src/components/Feedbacks/Feedbacks.jsx
@@ -1,33 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from './Options/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
 export function Feedbacks() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [positivePercentage, setPositivePercentage] = useState(0);
+  const [feedbacks, setFeedbacks] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  const handleGood = () => {
-    setGood(state => state + 1);
-  };
-  const handleNeutral = () => {
-    setNeutral(state => state + 1);
-  };
-  const handleBad = () => {
-    setBad(state => state + 1);
+  const handleFeedbacks = key => {
+    setFeedbacks(state => ({ ...state, [key]: state[key] + 1 }));
   };
 
-  useEffect(() => {
-    setTotal(good + neutral + bad);
-  }, [good, neutral, bad]);
-
-  useEffect(() => {
-    setPositivePercentage(parseFloat(((good / total) * 100).toFixed(1)));
-  }, [total, good]);
+  const { good, neutral, bad } = feedbacks;
+  const total = good + neutral + bad;
+  const positivePercentage = parseFloat(((good / total) * 100).toFixed(1));
 
   return (
     <>
@@ -39,9 +25,9 @@ export function Feedbacks() {
             onBad: 'Bad',
           }}
           onLeaveFeedback={{
-            onGood: handleGood,
-            onNeutral: handleNeutral,
-            onBad: handleBad,
+            onGood: () => handleFeedbacks('good'),
+            onNeutral: () => handleFeedbacks('neutral'),
+            onBad: () => handleFeedbacks('bad'),
           }}
         />
       </Section>
